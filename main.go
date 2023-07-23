f25e07439e5261705bf050ce19fa4ced3f1c4aa9
Eruditeruby <55877129+Eruditeruby@users.noreply.github.com>
2023-07-23 23:03:14 +0200

    Update main.go

--- main.go
+++ main.go
@@ -18,23 +18,23 @@ const (
 )
 
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello Ruby hi!</h1>"))
-}
+// func indexHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.Write([]byte("<h1>Hello Ruby hi!</h1>"))
+// }
 
 
 
 func main() {
-        fmt.Println("Hello Ruby2")  
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-	}
+ //        fmt.Println("Hello Ruby2")  
+	// port := os.Getenv("PORT")
+	// if port == "" {
+	// 	port = "3000"
+	// }
 
-	mux := http.NewServeMux()
+	// mux := http.NewServeMux()
 
-	mux.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":"+port, mux)
+	// mux.HandleFunc("/", indexHandler)
+	// http.ListenAndServe(":"+port, mux)
 
   // code for connection
 
