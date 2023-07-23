4af0bc6b7c00de75895df08be916b94904285cb2
Eruditeruby <55877129+Eruditeruby@users.noreply.github.com>
2023-07-24 00:21:21 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	// "net/http"
+	 "net/http"
+	"log"
 	// "os"
         "fmt"
 	 "database/sql"
@@ -18,12 +19,17 @@ const (
 )
 
 
+type sandbox struct {
+//id int
+name string
+age int
+}
 // func indexHandler(w http.ResponseWriter, r *http.Request) {
 // 	w.Write([]byte("<h1>Hello Ruby hi!</h1>"))
 // }
 
 
-
+var db *sql.DB
 func main() {
  //        fmt.Println("Hello Ruby2")  
 	// port := os.Getenv("PORT")
@@ -59,7 +65,53 @@ func main() {
        _, e := db.Exec(insertStmt)
        CheckError(e)
    }
- 
+
+func retrieveRecord(w http.ResponseWriter, r *http.Request) {
+
+// checks if the request is a "GET" request
+if r.Method != "GET" {
+http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+return
+}
+
+// We assign the result to 'rows'
+rows, err := db.Query("SELECT * FROM users")
+
+if err != nil {
+http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+return
+}
+defer rows.Close()
+
+
+// creates placeholder of the sandbox
+snbs := make([]sandbox, 0)
+
+
+// we loop through the values of rows
+for rows.Next() {
+snb := sandbox{}
+err := rows.Scan(&snb.name, &snb.age)
+if err != nil {
+log.Println(err)
+http.Error(w, http.StatusText(500), 500)
+return
+}
+snbs = append(snbs, snb)
+}
+
+if err = rows.Err(); err != nil {
+http.Error(w, http.StatusText(500), 500)
+return
+}
+
+// loop and display the result in the browser
+for _, snb := range snbs {
+fmt.Fprintf(w, "%d %s %s %d\n", snb.name, snb.age)
+}
+
+}
+
       func CheckError(err error) {
         if err != nil {
         panic(err)
