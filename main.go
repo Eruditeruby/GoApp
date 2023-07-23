efda9c0222e157779371b95a943a6c06c50b7464
Eruditeruby <55877129+Eruditeruby@users.noreply.github.com>
2023-07-23 20:32:39 +0200

    connect db

--- main.go
+++ main.go
@@ -4,11 +4,24 @@ import (
 	"net/http"
 	"os"
         "fmt"
+	 "database/sql"
+    _ "github.com/lib/pq"
 )
+
+const (
+    host     = "10.152.183.183"
+    port     = 5432
+    user     = "postgress"
+    password = "postgress"
+    dbname   = "user_db"
+)
+
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Hello Ruby hi!</h1>"))
 }
+
+
 
 func main() {
         fmt.Println("Hello Ruby2")  
@@ -21,5 +34,27 @@ func main() {
 
 	mux.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":"+port, mux)
-        
-}
+
+  // code for connection
+
+	 psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+         
+        //Open database
+         db, err := sql.Open("postgres", psqlconn)
+          CheckError(err)
+     
+        //Close database
+        defer db.Close()
+ 
+        // check db
+        err = db.Ping()
+        CheckError(err)
+  
+        fmt.Println("Connected!")
+   }
+ 
+      func CheckError(err error) {
+        if err != nil {
+        panic(err)
+       }
+  }
